f61c514edc70eecd35d507816b8ce9612b550af2
Israel Garcia <igarcia@nearsolutions.net>
2017-04-06 18:04:25 -0400

    Remove Custom Bouncy and add package Bouncy 1.8.1

--- ARSoft.CoreFX.Tools.Net/Properties/AssemblyInfo.cs
+++ ARSoft.CoreFX.Tools.Net/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ARSoft.CoreFX.Tools.Net")]
+[assembly: AssemblyProduct("ARSoft.Tools.NetCore")]
 [assembly: AssemblyTrademark("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
--- ARSoft.Tools.Net/Properties/AssemblyInfo.cs
+++ ARSoft.Tools.Net/Properties/AssemblyInfo.cs
@@ -1,37 +1,19 @@
-﻿#region Copyright and License
-// Copyright 2010..2016 Alexander Reinert
-// 
-// This file is part of the ARSoft.Tools.Net - C# DNS client/server and SPF Library (http://arsofttoolsnet.codeplex.com/)
-// 
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-// 
-//   http://www.apache.org/licenses/LICENSE-2.0
-// 
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-#endregion
-
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-using System.Text;
 
-[assembly: AssemblyTitle("ARSoft.Tools.Net")]
-[assembly: AssemblyDescription("")]
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Alexander Reinert")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ARSoft.Tools.Net")]
-[assembly: AssemblyCopyright("Copyright © Alexander Reinert 2010..2016")]
 [assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-[assembly: Guid("7c85a543-2ba0-477d-8e77-0d080cc03c5d")]
-[assembly: AssemblyVersion("2.2.6.0")]
-[assembly: AssemblyFileVersion("2.2.6.0")]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9c3c1c83-31b8-415c-bb60-edeeef2834ea")]
